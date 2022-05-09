7380b80d73585d246ee0c85bfabc2112887f59a9
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-05-09 13:17:14 -0400

    made board and gave initial style and window layout

--- memory.js
+++ memory.js
@@ -1,5 +1,6 @@
+//setting up the DOM? Not sure why they do it like this??
 document.addEventListener('DOMContentLoaded', () => {
-    //card options
+    //card options array with each card an object
     const cardArray = [
         {
             name: 'crow',
@@ -17,58 +18,122 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'beeFlying',
             img: 'images/beeFlying.jpg'
         },
+        
         {
             name: 'beePassion',
             img: 'images/beePassion.jpg'
         },
         {
-            name: 'black',
-            img: 'images/black.jpg'
+            name: 'beePassion',
+            img: 'images/beePassion.jpg'
         },
+       
         {
-            name: '',
+            name: 'bwDrop',
             img: 'images/bwDrop.jpg'
         },
         {
-            name: '',
-            img: 'images/cardBlue.jpg'
+            name: 'bwDrop',
+            img: 'images/bwDrop.jpg'
         },
+        
         {
-            name: '',
-            img: 'images/cardGreen.jpg'
-        },
-        {
-            name: '',
+            name: 'flower1',
             img: 'images/flower1.jpg'
         },
         {
-            name: '',
+            name: 'flower1',
+            img: 'images/flower1.jpg'
+        },
+        {
+            name: 'frit2',
             img: 'images/frit2.jpg'
         },
         {
-            name: '',
+            name: 'frit2',
+            img: 'images/frit2.jpg'
+        },
+        {
+            name: 'hopper2',
             img: 'images/hopper2.jpg'
         },
         {
-            name: '',
+            name: 'hopper2',
+            img: 'images/hopper2.jpg'
+        },
+        {
+            name: 'inverseFlower',
             img: 'images/inverseFlower.jpg'
         },
         {
-            name: '',
+            name: 'inverseFlower',
+            img: 'images/inverseFlower.jpg'
+        },
+        {
+            name: 'paradiseDew',
             img: 'images/paradiseDew.jpg'
         },
         {
-            name: '',
+            name: 'paradiseDew',
+            img: 'images/paradiseDew.jpg'
+        },
+        {
+            name: 'spider',
             img: 'images/spider.jpg'
         },
         {
-            name: '',
+            name: 'spider',
+            img: 'images/spider.jpg'
+        },
+        {
+            name: 'StigmaHeart',
             img: 'images/StigmaHeart.jpg'
         },
         {
-            name: '',
-            img: ''
+            name: 'StigmaHeart',
+            img: 'images/StigmaHeart.jpg'
         },
+        {
+            name: 'coral',
+            img: 'images/coral.jpg'
+        },
+        {
+            name: 'coral',
+            img: 'images/coral.jpg'
+        },
+        {
+            name: 'mothCutie',
+            img: 'images/mothCutie.jpg'
+        },
+        {
+            name: 'mothCutie',
+            img: 'images/mothCutie.jpg'
+        },
+        {
+            name: 'wing',
+            img: 'images/wing.jpg'
+        },
+        {
+            name: 'wing',
+            img: 'images/wing.jpg'
+        }
     ]
+// select grid
+const grid = document.querySelector('.grid')
+//create the game board. 1. for-loop for each card. 
+// 2.create the img element for each card, giving it name 'card'.
+// 3. set attribute to link it to the blank, set both it's name/local and an data-id name 1-end of array.
+// 4. add event listener have been clicked on and invoke a flip card function. 
 
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++)
+    {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/cardGreen.jpg')
+        card.setAttribute('data-id', i)
+        // card.addEventListener('click,'flipCard)
+        grid.appendChild(card)
+    }
+}
+createBoard()
 })
