d31aabc55400c9d24a138489ec284ff073f7a34a
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-05-09 11:49:47 -0400

    setting up space and added photos

---
+++ memory.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+        {
+            name: 'crow',
+            img: 'images/crow.jpg'
+        },
+        {
+            name: 'crow',
+            img: 'images/crow.jpg'
+        },
+        {
+            name: 'beeFlying',
+            img: 'images/beeFlying.jpg'
+        },
+        {
+            name: 'beeFlying',
+            img: 'images/beeFlying.jpg'
+        },
+        {
+            name: 'beePassion',
+            img: 'images/beePassion.jpg'
+        },
+        {
+            name: 'black',
+            img: 'images/black.jpg'
+        },
+        {
+            name: '',
+            img: 'images/bwDrop.jpg'
+        },
+        {
+            name: '',
+            img: 'images/cardBlue.jpg'
+        },
+        {
+            name: '',
+            img: 'images/cardGreen.jpg'
+        },
+        {
+            name: '',
+            img: 'images/flower1.jpg'
+        },
+        {
+            name: '',
+            img: 'images/frit2.jpg'
+        },
+        {
+            name: '',
+            img: 'images/hopper2.jpg'
+        },
+        {
+            name: '',
+            img: 'images/inverseFlower.jpg'
+        },
+        {
+            name: '',
+            img: 'images/paradiseDew.jpg'
+        },
+        {
+            name: '',
+            img: 'images/spider.jpg'
+        },
+        {
+            name: '',
+            img: 'images/StigmaHeart.jpg'
+        },
+        {
+            name: '',
+            img: ''
+        },
+    ]
+
+})
