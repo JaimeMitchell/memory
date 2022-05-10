92145144a4f56026881ff858200530697fc39072
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-05-10 11:40:02 -0400

    Game works. Started working on style.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 //setting up the DOM? Not sure why they do it like this??
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
     //card options array with each card an object
     const cardArray = [
         
@@ -79,60 +79,64 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/spider.jpg'
         },
        
-        {
-            name: 'coral',
-            img: 'images/coral.jpg'
-        },
-        {
-            name: 'coral',
-            img: 'images/coral.jpg'
-        },
-        {
-            name: 'mothCutie',
-            img: 'images/mothCutie.jpg'
-        },
-        {
-            name: 'mothCutie',
-            img: 'images/mothCutie.jpg'
-        },
-        {
-            name: 'wing',
-            img: 'images/wing.jpg'
-        },
-        {
-            name: 'wing',
-            img: 'images/wing.jpg'
-        }
+        // {
+        //     name: 'coral',
+        //     img: 'images/coral.jpg'
+        // },
+        // {
+        //     name: 'coral',
+        //     img: 'images/coral.jpg'
+        // },
+        // {
+        //     name: 'mothCutie',
+        //     img: 'images/mothCutie.jpg'
+        // },
+        // {
+        //     name: 'mothCutie',
+        //     img: 'images/mothCutie.jpg'
+        // },
+        // {
+        //     name: 'wing',
+        //     img: 'images/wing.jpg'
+        // },
+        // {
+        //     name: 'wing',
+        //     img: 'images/wing.jpg'
+        // }
     ]
-
     cardArray.sort(() => 0.5 - Math.random())
     // select grid
     const grid = document.querySelector('.grid')
     const resultDisplay = document.querySelector('#result')
-    let cardsChosen = []
-    let cardsChosenId = []
+
     let cardsWon = []
     //create the game board. 1 . for-loop for each card. 
     // 2.create the img element for each card, giving it name 'card'.
     // 3. set attribute to link it to the blank, set both it's name/local and an data-id name 1-end of array.
     // 4. add event listener have been clicked on and invoke a flip card function. 
     //flip the card
-    function flipCard() {
-        //check console log and take .this out to see what happens.
-        let cardId = this.getAttribute('data-id')
-        cardsChosen.push(cardArray[cardId].img)
-        cardsChosenId.push(cardId)
-        this.setAttribute('src', cardArray[cardId].img)
-        if (cardsChosen.length === 2) { setTimeout(checkForMatch, 500) }
-    }
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img')
             card.setAttribute('src', 'images/cardGreen.jpg')
             card.setAttribute('data-id', i)
-            card.addEventListener('click', flipCard)
             grid.appendChild(card)
+            card.addEventListener('click', flipCard)
+           
+
         }
+    }
+    let cardsChosen = []
+    let cardsChosenId = []
+    function flipCard() {console.log(this)
+        //check console log and take .this out to see what happens.
+        let cardId = this.getAttribute('data-id')
+        console.log(cardId)
+        cardsChosen.push(cardArray[cardId].img)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {setTimeout(checkForMatch, 500)
+        console.log(this) }
     }
     //check for matches
     function checkForMatch() {
@@ -140,14 +144,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const optionOneId = cardsChosenId[0]
         const optionTwoId = cardsChosenId[1]
         if (cardsChosen[0] === cardsChosen[1]) {
-            alert("MATCH")
+           
             cards[optionOneId].setAttribute('src', 'images/black.jpg')
             cards[optionTwoId].setAttribute('src', 'images/black.jpg')
             cardsWon.push(cardsChosen)
         } else {
-            cards[optionOneId].setAttribute('src', images / cardGreen.jpg)
-            cards[optionTwoId].setAttribute('src', images / cardGreen.jpg)
-            alert('nope')
+            cards[optionOneId].setAttribute('src', "images/cardGreen.jpg")
+            cards[optionTwoId].setAttribute('src', "images/cardGreen.jpg")
+           
         }
         cardsChosen = []
         cardsChosenId = []
@@ -158,4 +162,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     createBoard()
-})
+// })
