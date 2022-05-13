2c8fcee6c0d4320af569727333d394f50c7f9d2f
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-05-13 09:29:49 -0400

    deleted files

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@
 4. add event listener have been clicked on and invoke a flip card function. 
     flip the card*/
 
-    //still not sure what this does.
+//still not sure what this does.
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     //card options array with each card an object, note the curly brackets
     const cardArray = [
         {
@@ -83,75 +83,46 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/200px-7.jpg'
         }
     ]
-    //Randomly 
+    //1. Random card layout on each replay. 
     cardArray.sort(() => 0.5 - Math.random())
     const grid = document.querySelector('.grid')
     const resultDisplay = document.querySelector('#result')
-    // cards chosen
+
+    //2. cards chosen array will have 2 cards pushed to it if their images match
     let cardsChosen = []
-    // Array of cards chosen with an id number 0-27, this is used to make sure the compared cards do not have matching IDs
+
+    //3. Originally just for changing attribute of matching cards to hidden, but I also used it to make sure the compared cards do not have matching IDs
     let cardsChosenId = []
-    //Array of matching pairs have same path but different id numbers, used to keep score, and end came
+
+    //4. Array of matching pairs have same path but different id numbers, used to keep score, and end game
     let cardsWon = []
-    
+
     function createBoard() {
-        //Loop through card array
+        //1. Loop through card array
         for (let i = 0; i < cardArray.length; i++) {
-            //Creat an image element named card of EACH object in array.
-            let card = document.createElement('img') // changed from var to let
-            //Set EACH card to it's src attribute (Images Relative Path) AND to the green card (virtual back side). Why are they set together and the ID is set separately?
+
+            //2. Creat an image element named card of EACH object in array.
+            let card = document.createElement('img')// changed from var to let
+
+            //3. Set EACH card to it's src attribute (Images Relative Path) AND to the green card (virtual back side). Why are they set together and the ID is set separately?
             card.setAttribute('src', 'images/MemoryBackground-11.jpg')
-            // Set EACH card to a data idea #0-27 using the i iterator
+
+            //4. Set EACH card to a data idea #0-27 using the i iterator
             card.setAttribute('data-id', i)
-            //append the card to the grid. Putting after creating card did nothing
+
+            //5. Append the card to the grid. Putting after creating card did nothing
             grid.appendChild(card)
-            //turning card event
+
+            //6. Turning card event
             card.addEventListener('click', flipCard)
-        }
-    }
-    //CHECK FOR MATCHES within FLIPCARD if-statement."are there two cards to compare?"
-    function checkForMatch() {
-        //New variable to select all images created in the DOM. Note PLURAL cards is not card variable (above)
-        let cards = document.querySelectorAll('img')
-        //New variables to set cards compared to green or black. Not to compare but only used to change their attributes below.
-        const optionOneId = cardsChosenId[0]
-        const optionTwoId = cardsChosenId[1]
-
-        if (cardsChosen[0] === cardsChosen[1] && cardsChosenId[0] !== cardsChosenId[1]) {   cards[optionOneId].style.visibility = 'hidden'
-            cards[optionTwoId].style.visibility = 'hidden'
-            // cards[optionOneId].setAttribute('src', 'images/Black200px.jpg')
-            // cards[optionTwoId].setAttribute('src', 'images/Black200px.jpg')
-            //pushes matching cardsChosen[0] and cardsChosen[1] to cardsWon Array in order to tally score
-            cardsWon.push(cardsChosen)
-            //TRY THIS //change cardsWon to a sum instead of array, remove child from parent so can't be turned back over, plus black background won't be required and can change backgrounds at will.
-
-        }
-        else {
-            cards[optionOneId].setAttribute('src', 'images/MemoryBackground-12.jpg')
-            cards[optionTwoId].setAttribute('src', 'images/MemoryBackground-13.jpg')
-
-        }
-        // if(cardsChosen[0]=== cardsChosen[0] || cardsChosen[1]===cardsChosen[1] ||cardsChosenId[0]===cardsChosenId[0] || cardsChosenId[1]===cardsChosenId[1]) {
-        //     return }
-
-        //resets array so cards flip back to greenBack.jpg or black.jpg
-        cardsChosen = []
-        cardsChosenId = []
-        //adding one each time to the score board for each matched pair which equals one nested array within cardsWon array. TRY THIS: If I end up removing cardsWon array, make sure to return it to a sum so this works. Perhaps the return value of pop()
-        resultDisplay.textContent = cardsWon.length
-        //cardsWon length is half of CardArray length because 2 cards are make one nested array so need to divide the cardArray.
-        if (cardsWon.length === cardArray.length / 2) {
-            resultDisplay.textContent = "WON'n'DONE"
         }
     }
     function flipCard() {
 
         //1.Attach the value 'this ID attribute on the button clicked' to cardId
-
         let cardId = this.getAttribute('data-id')
 
         //2.Push the cardArray[dynamic index] and it's image to CardsChosen Array
-
         cardsChosen.push(cardArray[cardId].img)
 
         //3. Push the id attributed card to cardsChosenId ARRAY
@@ -166,7 +137,45 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(this)
         }
     }
-    //Randomly create the board
+
+    function checkForMatch() {
+         
+        //1. New variable to select all images created in the DOM. Note PLURAL cards is not card variable (above)
+        let cards = document.querySelectorAll('img')
+
+        // 2. New variables to set cards. Not to compare but to hide cards when matched.
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+
+        // 3. If flipped cards image src are equal AND if their IDs are different they match
+        if (cardsChosen[0] === cardsChosen[1] && cardsChosenId[0] !== cardsChosenId[1]) {
+
+            cards[optionOneId].style.visibility = 'hidden'
+            cards[optionTwoId].style.visibility = 'hidden'
+            // cards[optionOneId].setAttribute('src', 'images/Black200px.jpg')
+            // cards[optionTwoId].setAttribute('src', 'images/Black200px.jpg')
+            //pushes matching cardsChosen[0] and cardsChosen[1] to cardsWon Array in order to tally score
+            cardsWon.push(cardsChosen)
+            //TRY THIS //change cardsWon to a sum instead of array, remove child from parent so can't be turned back over, plus black background won't be required and can change backgrounds at will.
+
+        }
+        else {
+            cards[optionOneId].setAttribute('src', 'images/MemoryBackground-11.jpg')
+            cards[optionTwoId].setAttribute('src', 'images/MemoryBackground-11.jpg')
+
+        }
+        // if(cardsChosen[0]=== cardsChosen[0] || cardsChosen[1]===cardsChosen[1] ||cardsChosenId[0]===cardsChosenId[0] || cardsChosenId[1]===cardsChosenId[1]) {
+        //     return }
+
+        //resets array so cards flip back to greenBack.jpg or black.jpg
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        //cardsWon length is half of CardArray length because 2 cards make one nested array so need to divide the cardArray.
+        if (cardsWon.length === cardArray.length / 2) {
+            resultDisplay.textContent = "WON'n'DONE"
+        }
+    }
     createBoard()
 
 })
